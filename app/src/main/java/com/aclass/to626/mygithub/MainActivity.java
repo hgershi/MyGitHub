18b47022d2e8ee627db7aa4ee6cca89f62ffae77
hgershi <hgershon@umich.edu>
2016-11-16 09:19:37 -0500

    abc

---
+++ app/src/main/java/com/aclass/to626/mygithub/MainActivity.java
@@ -0,0 +1,13 @@
+package com.aclass.to626.mygithub;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+//change things
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
